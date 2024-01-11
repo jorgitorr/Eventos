215ebf581352e6d33eb72a369c7bf2ff68d2b7cf
jorgitorr <jorgitorr97@gmail.com>
2024-01-11 18:27:31 +0100

    nuevo

--- Eventos/Program.cs
+++ Eventos/Program.cs
@@ -6,7 +6,19 @@ internal class Program
         
     public static void Main(string[] args)
     {
-        Action miEvento = () => { };
+        Ejemplo<int> miEjemplo = new Ejemplo<int>();
+        miEjemplo.setPropiedad(6);
+        Ejemplo<string> miEjemplo2 = new Ejemplo<string>();
+        miEjemplo2.setPropiedad("hola");
+        Ejemplo2<int, string> miEjemplo3 = new Ejemplo2<int, string>();
+        miEjemplo3.setPropiedad(5,"manzanas");
+        Console.WriteLine($"El tipo de la clase Ejemplo2 es: {miEjemplo3}");
+        
+        Action<int> miEventoParametrizado = null;
+        Action miEvento = null;
+        miEventoParametrizado += (int x) => Console.WriteLine(x);
+        
+        miEvento = () => { };
         miEvento += () => { Console.WriteLine("Evento Lanzado"); };
         miEvento += () => { Console.WriteLine("Evento 2 Lanzado"); };
         miEvento.Invoke();
@@ -28,6 +40,40 @@ internal class Program
         void miParametrizado(int numero)
         {
             Console.WriteLine("Metodo " + numero + " lanzado");
+        }
+        
+    }
+    
+    /*La T es un genérico, que son variables las cuales se pone el tipo una vez se crea el objeto*/
+    class Ejemplo<T>
+    {
+        private T propiedad;
+
+        public void setPropiedad(T p)
+        {
+            propiedad = p;
+        }
+
+        public override string ToString()
+        {
+            return typeof(T).ToString();
         } 
+    }
+
+    class Ejemplo2<T,T2>
+    {
+        private T propiedad1;
+        private T2 propiedad2;
+
+        public void setPropiedad(T p, T2 p2)
+        {
+            propiedad1 = p;
+            propiedad2 = p2;
+        }
+
+        public override string ToString()
+        {
+            return typeof(T).ToString() + typeof(T2).ToString();
+        }
     }
 }
