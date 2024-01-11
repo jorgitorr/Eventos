6d779edf5385bb6400438859086be98892c11eca
jorgitorr <jorgitorr97@gmail.com>
2024-01-11 15:57:07 +0100

    nuevo

---
+++ Eventos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+internal class Program
+{
+        
+    public static void Main(string[] args)
+    {
+        Action miEvento = () => { };
+        miEvento += () => { Console.WriteLine("Evento Lanzado"); };
+        miEvento += () => { Console.WriteLine("Evento 2 Lanzado"); };
+        miEvento.Invoke();
+        miEvento += () => { Console.WriteLine("Evento 3 lanzado"); };
+        miEvento += () => { Console.WriteLine("Evento 4 lanzado"); };
+        miEvento += miMetodo;
+        Action<int> nuevoEvento = (int numero) => { };
+        nuevoEvento += miParametrizado; /*-> es el metodo parametrizado*/ 
+        nuevoEvento.Invoke(6);
+        Console.WriteLine("---2 LLAMADA----");
+        miEvento.Invoke();
+            
+            
+        void miMetodo()
+        {
+            Console.WriteLine("Metodo 5 lanzado");
+        }
+
+        void miParametrizado(int numero)
+        {
+            Console.WriteLine("Metodo " + numero + " lanzado");
+        } 
+    }
+}
